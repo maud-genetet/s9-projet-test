4fd61055960719e50d62198d119a59537def367c
Maud Genetet <maud.genetet@enseirb-matmeca.fr>
2025-12-11 11:22:37 +0100

    debut projet

---
+++ agence-voyage/src/main/java/com/agence/App.java
@@ -0,0 +1,13 @@
+package com.agence;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
